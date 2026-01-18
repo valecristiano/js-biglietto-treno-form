dc1352f95dedd4f61238c36ec14ad1835234e824
valecristiano <valeriamdcristiano@gmail.com>
2026-01-18 15:45:35 +0300

    Milestone 2 first part

--- script.js
+++ script.js
@@ -4,15 +4,25 @@ const nomePassegero = document.getElementById("ticket-name");
 const kmPercorsi = document.getElementById("ticket-km");
 const etaPasseggero = document.getElementById("ticket-age");
 
+// Variabili ticket
+
+const passengerTicket = document.getElementById("ticket");
+const passengerNAme = document.querySelector(".passenger-name");
+const passengerPromo = document.querySelector(".passenger-promo");
+const passengerSeat = document.querySelector(".passenger-seat");
+const passengerCode = document.querySelector(".passenger-code");
+const passengerPrice = document.querySelector(".passenger-price");
+
+// Variabili prezzi
 const prezzoPerKm = 0.21;
 const scontoMinorenni = 20;
 const scontoOver65 = 40;
 let prezzoFinale = 0;
 
+// Funzione calcolo costo biglietti
+
 formBiglietti.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  //   const kmRichiesti = parseInt(kmPercorsi.value);
 
   if (etaPasseggero.value === "Minorenne") {
     const prezzoPieno = kmPercorsi.valueAsNumber * prezzoPerKm;
@@ -29,4 +39,20 @@ formBiglietti.addEventListener("submit", (event) => {
   console.log("kmPercorsi", kmPercorsi.value);
   console.log("età", etaPasseggero.value);
   console.log("prezzoFinale", prezzoFinale);
+
+  if (prezzoFinale) {
+    passengerTicket.classList.remove("d-none");
+    passengerNAme.innerText = `${nomePassegero.value}`;
+    passengerPromo.innerText = 0;
+    passengerSeat.innerText = 0;
+    passengerCode.innerText = 0;
+    passengerPrice.innerText = `${prezzoFinale.toFixed(2)}€`;
+  }
 });
+
+// const annulla = document.getElementById("annulla");
+
+// annulla.addEventListener("click", () => {
+//   formBiglietti.reset();
+//   passengerTicket.classList.add("d-none");
+// });
